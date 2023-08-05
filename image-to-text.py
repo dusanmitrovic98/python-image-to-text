b0e72c4b8d7ed5a54732c9cd69f4d4925f828f3f
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-05 19:39:06 +0200

    Update image-to-text.py

--- image-to-text.py
+++ image-to-text.py
@@ -8,3 +8,4 @@ def extract_text_from_image(image_path):
         image = cv2.imread(image_path)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         extracted_text = pytesseract.image_to_string(gray_image)
+        return extracted_text.strip()
