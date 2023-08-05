5faece2ae8b78974936029b92186cd30673a1507
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-05 19:38:34 +0200

    Update image-to-text.py

--- image-to-text.py
+++ image-to-text.py
@@ -6,3 +6,4 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tess
 def extract_text_from_image(image_path):
     try:
         image = cv2.imread(image_path)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
